109a6eb442791f6be2410909e9b751cadc662981
Roman Birg <roman@cyngn.com>
2015-06-10 10:34:35 -0700

    Allow permissions to be granted via whitelisted signatures
    When an application defines a permission, it can now add a new
    attribute "allowViaWhitelist", a boolean value. If set to true,
    the permission may be granted to a package signed with a predefined key,
    if it is defined via <allow-permission> in
    /system/etc/permissions/someapp.xml.
    
    Since this is a hidden attribute, it must use the prv namespace XML
    declaration. E.g.: add the following to the <manifest> tag:
    
           xmlns:androidprv="http://schemas.android.com/apk/prv/res/android"
    
    In the permission declaration:
    
            <permission
                android:name=""
                android:protectionLevel="signature"
                androidprv:allowViaWhitelist"true" />
    
    And a corresponding entry in /system/etc/permissions/someapp.xml:
    
            <allow-permission
                name="some.android.PERMISSION"
                signature="<known public signature>" />
    
    Note: if the permission never declares "allowViaWhitelist", then the
    whitelisted permissions will be ignored.
    
    Change-Id: Ie4597a07eb0a193375fa2724bd9cf468184a7926
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -2462,6 +2462,9 @@ final class Settings {
                     }
                 }
             }
+            if (bp.allowViaWhitelist) {
+                serializer.attribute(null, "allowViaWhitelist", Integer.toString(1));
+            }
             serializer.endTag(null, TAG_ITEM);
         }
     }
@@ -3115,6 +3118,8 @@ final class Settings {
                     bp.protectionLevel = readInt(parser, null, "protection",
                             PermissionInfo.PROTECTION_NORMAL);
                     bp.protectionLevel = PermissionInfo.fixProtectionLevel(bp.protectionLevel);
+                    bp.allowViaWhitelist = readInt(parser, null,
+                            "allowViaWhitelist", 0) == 1;
                     if (dynamic) {
                         PermissionInfo pi = new PermissionInfo();
                         pi.packageName = sourcePackage.intern();
@@ -3122,6 +3127,7 @@ final class Settings {
                         pi.icon = readInt(parser, null, "icon", 0);
                         pi.nonLocalizedLabel = parser.getAttributeValue(null, "label");
                         pi.protectionLevel = bp.protectionLevel;
+                        pi.allowViaWhitelist = bp.allowViaWhitelist;
                         bp.pendingInfo = pi;
                     }
                     out.put(bp.name, bp);
