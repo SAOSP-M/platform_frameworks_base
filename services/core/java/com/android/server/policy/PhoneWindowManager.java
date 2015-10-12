c253ff5989f76c1ecfc842fa46093bea9a9f26e0
Blefish <rtammekivi@gmail.com>
2015-10-12 17:48:39 +0300

    policy: Show a simple dialog about optimizing apps
    When booting, new apps are optimized with dex2oat. During this process a
    progress dialog is displayed (optimizing app 13/74). This dialog has an
    indetemninate progress spinner that uses 50% of cpu.
    
    This leaves only 50% for dex2oat, prolonging the time to boot.
    
    Ported to Marshmallow by @KreAch3R
    
    Change-Id: I8c39b54a3aefc03513d30c6478bca9bbe3ddbe04

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -24,7 +24,7 @@ import android.app.ActivityManagerNative;
 import android.app.IActivityManager;
 import android.app.AppOpsManager;
 import android.app.IUiModeManager;
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.app.StatusBarManager;
 import android.app.UiModeManager;
@@ -6331,7 +6331,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         screenTurningOn(null);
     }
 
-    ProgressDialog mBootMsgDialog = null;
+    AlertDialog mBootMsgDialog = null;
 
     /**
      * name of package currently being dex optimized
@@ -6362,7 +6362,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         theme = 0;
                     }
 
-                    mBootMsgDialog = new ProgressDialog(mContext, theme) {
+                    mBootMsgDialog = new AlertDialog(mContext, theme) {
                         // This dialog will consume all events coming in to
                         // it, to avoid it trying to do things too early in boot.
                         @Override public boolean dispatchKeyEvent(KeyEvent event) {
@@ -6390,8 +6390,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     } else {
                         mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
-                    mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                    mBootMsgDialog.setIndeterminate(true);
                     mBootMsgDialog.getWindow().setType(
                             WindowManager.LayoutParams.TYPE_BOOT_PROGRESS);
                     mBootMsgDialog.getWindow().addFlags(
@@ -6402,6 +6400,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     lp.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR;
                     mBootMsgDialog.getWindow().setAttributes(lp);
                     mBootMsgDialog.setCancelable(false);
+		    mBootMsgDialog.setMessage("");
                     mBootMsgDialog.show();
                 }
 
