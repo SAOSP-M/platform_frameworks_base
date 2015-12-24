640b533c11e53d2284866366c6cafd0527bf0e63
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-12-24 02:11:38 +0100

    Lock screen: Add music visualizer, (1/2):
    Based on CyanogenMod:
    SystemUI: use new visualizer from Eleven:
    https://github.com/CyanogenMod/android_frameworks_base/commit/19e458f4a26fe7c8b6419cadba81a0c46dc79dad
    
    SystemUI: fix NPE crash when Visualizer is not initialized:
    https://github.com/CyanogenMod/android_frameworks_base/commit/a41cb45a5d8dbce3cce3b6d3e9ef8beee17b5a31
    
    SystemUI: hide visualizer when keyguard is occluded:
    https://github.com/CyanogenMod/android_frameworks_base/commit/44a3303477d114fcfad90bc03ae88f028006c26a
    
    Included:
    - Option to show/hide the music visualizer
    - Option to use a custom color instead of the color created from the lock screen background
    - Custom color

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3510,6 +3510,24 @@ public final class Settings {
                 new InclusiveIntegerRangeValidator(0, 2);
 
         /**
+         *  Weather to show the music visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_SHOW = "lock_screen_visualizer_show";
+
+        /**
+         *  Weather to use a custom color for the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_USE_CUSTOM_COLOR = "lock_screen_visualizer_use_custom_color";
+
+        /**
+         *  Custom color of the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
+
+        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
