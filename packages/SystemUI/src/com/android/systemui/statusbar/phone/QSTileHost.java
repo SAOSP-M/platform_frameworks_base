CyanogenMod/android_frameworks_base

QSTiles: use host to reset tiles instead of receiver
Change-Id: Icebe022826a623d37b7c82e07021c24b38a52565
Signed-off-by: Roman Birg <roman@cyngn.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -331,6 +331,13 @@ public class QSTileHost implements QSTile.Host, Tunable {
                 TextUtils.join(",", tiles), ActivityManager.getCurrentUser());
     }
 
+    @Override
+    public void resetTiles() {
+        setEditing(false);
+        CMSettings.Secure.putStringForUser(getContext().getContentResolver(),
+                CMSettings.Secure.QS_TILES, "default", ActivityManager.getCurrentUser());
+    }
+
     public static int getLabelResource(String spec) {
         if (spec.equals("wifi")) return R.string.quick_settings_wifi_label;
         else if (spec.equals("bt")) return R.string.quick_settings_bluetooth_label;

SAOSP-M/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -336,6 +336,13 @@ public class QSTileHost implements QSTile.Host, Tunable {
                 TextUtils.join(",", tiles), ActivityManager.getCurrentUser());
     }
 
+    @Override
+    public void resetTiles() {
+        setEditing(false);
+        Settings.Secure.putStringForUser(getContext().getContentResolver(),
+                Settings.Secure.QS_TILES, "default", ActivityManager.getCurrentUser());
+    }
+
     public static int getLabelResource(String spec) {
         if (spec.equals("wifi")) return R.string.quick_settings_wifi_label;
         else if (spec.equals("bt")) return R.string.quick_settings_bluetooth_label;