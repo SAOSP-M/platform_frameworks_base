a162ac32536207abd6aca05719c4a6f9b0a5385e
beanstown106 <nbenis106@gmail.com>
2016-01-31 17:20:58 -0500

    Fix issue where ongoing lockscreen notifications were off by default *also added missing enforceSystemOrSystemUI bit from lp

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1334,6 +1334,7 @@ public class NotificationManagerService extends SystemService {
 
         @Override
         public int getShowNotificationForPackageOnKeyguard(String pkg, int uid) {
+            enforceSystemOrSystemUI("INotificationManager.getShowNotificationForPackageOnKeyguard");
             return mRankingHelper.getShowNotificationForPackageOnKeyguard(pkg, uid);
         }
 
