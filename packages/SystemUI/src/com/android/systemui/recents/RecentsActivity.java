1819509f4f56ac1d19264ed2b8ad16f26e4069f0
snak3ater <saeedyasir91@gmail.com>
2016-02-10 12:46:42 +0500

    SystemUI: Remove all recents searchbar patches
    This reverts commit 1c2247fa67470dfb504710bb9bfd861331bbbd5b.
    This reverts commit 8cd71b5f2cf6d3984fbbed09505cd2bdff8e96e3.
    This reverts commit a25edbb5450557d3125a1b5fdb92c5b3d1ca83ac.

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -25,11 +25,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewStub;
@@ -38,7 +36,6 @@ import android.widget.Toast;
 import com.android.internal.logging.MetricsLogger;
 import com.android.systemui.Prefs;
 import com.android.systemui.R;
-import com.android.systemui.recents.RecentsConfiguration;
 import com.android.systemui.recents.misc.Console;
 import com.android.systemui.recents.misc.DebugTrigger;
 import com.android.systemui.recents.misc.ReferenceCountedTrigger;
@@ -132,7 +129,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     /**
      * Broadcast receiver to handle messages from AlternateRecentsComponent.
      */
-    private final BroadcastReceiver mServiceBroadcastReceiver = new BroadcastReceiver() {
+    final BroadcastReceiver mServiceBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -162,7 +159,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     /**
      * Broadcast receiver to handle messages from the system
      */
-    private final BroadcastReceiver mSystemBroadcastReceiver = new BroadcastReceiver() {
+    final BroadcastReceiver mSystemBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -181,7 +178,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     /**
      * A custom debug trigger to listen for a debug key chord.
      */
-    private final DebugTrigger mDebugTrigger = new DebugTrigger(new Runnable() {
+    final DebugTrigger mDebugTrigger = new DebugTrigger(new Runnable() {
         @Override
         public void run() {
             onDebugModeTriggered();
@@ -266,20 +263,10 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
             }
 
             findViewById(R.id.floating_action_button).setVisibility(View.VISIBLE);
-        
-            boolean showSearchBar = Settings.System.getInt(getContentResolver(),
-                       Settings.System.RECENTS_SHOW_SEARCH_BAR, 1) == 1;
-
-            if (mRecentsView.hasValidSearchBar()) {
-                if (showSearchBar) {
-                    mRecentsView.setSearchBarVisibility(View.VISIBLE);
-                } else {
-                    mRecentsView.setSearchBarVisibility(View.GONE);
-                }
+            if (mRecentsView.hasValidSearchBar()) {           
+                mRecentsView.setSearchBarVisibility(View.VISIBLE);
             } else {
-                if (showSearchBar) {
-                    refreshSearchWidgetView();
-                }
+                refreshSearchWidgetView();
             }
         }
 
